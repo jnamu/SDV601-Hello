251339ef2e4479252a4f73b2f621f999063c3221
Rebekah <rebekah.rossiter@gmail.com>
2015-05-31 11:20:56 +1200

    Added
    Added files to SDV601 Hello. Can't remember what project this is

---
+++ Hello/FrmWelcome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hello
+{
+    public partial class FrmWelcome : Form
+    {
+        public FrmWelcome()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnHello_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("C# rocks!", "Welcome",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+    }
+}
